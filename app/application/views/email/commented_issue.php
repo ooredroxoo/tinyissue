2c766a6aecc6b4e11a8f3cf0877bbc6d5f8db643
Rafael Nascimento Sampaio <rafaelns@brazcubas.br>
2013-11-25 17:04:56 -0200

    Improved message

--- app/application/views/email/commented_issue.php
+++ app/application/views/email/commented_issue.php
@@ -1,3 +1,3 @@
-<h1>Chamado <?php echo $issue_id; ?> <?php echo $issue; ?> atualizado.</h1>
+<h2>Chamado <?php echo $issue_id; ?> - <?php echo $issue; ?> atualizado.</h2>
 <p>Olá <?php echo $firstname ?>,<br>O chamado <?php echo $issue_id; ?> recebeu um novo comentário de <strong><?php echo $author; ?></strong>. Veja abaixo.</p>
 <p><?php echo $comment; ?></p>
